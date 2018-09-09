9d9b8be6ce6bec8ee549188cb9887c0970f9ec84
rafael <rafaelxpxpxp@gmail.com>
2018-09-09 18:38:01 -0300

    Init commit

---
+++ src/main/java/logs/MethodLogger.java
@@ -0,0 +1,34 @@
+package logs;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Log methods args and retuning by default
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MethodLogger {
+
+    /**
+     * Put a message in the log if it  was a success
+     */
+    String msgOnSuccess() default "";
+
+    /**
+     * Put a message in the log if it  was a error
+     */
+    String msgOnException() default "";
+
+    /**
+     * Tell if it should log its args
+     */
+    boolean logAllArgs() default true;
+
+    /**
+     * Tell if it should log its return
+     */
+    boolean logReturn() default true;
+}
---
+++ src/main/java/logs/MethodLoggerAspect.java
@@ -0,0 +1,130 @@
+package logs;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.stream.Stream;
+
+
+@Aspect
+@Configuration
+public class MethodLoggerAspect {
+
+    @Before(value = "@annotation(methodLogger))")
+    public void methodEntryLogger(JoinPoint joinPoint, MethodLogger methodLogger){
+        StringBuilder logBuilder = new StringBuilder();
+        logMethod(joinPoint, logBuilder);
+        logArgs(joinPoint, logBuilder, methodLogger);
+        createLog(joinPoint, logBuilder);
+    }
+
+
+    @AfterReturning(value = "@annotation(methodLogger))", returning = "returningValue")
+    public void methodReturnLogger(JoinPoint joinPoint, MethodLogger methodLogger, Object returningValue) {
+        if (hasReturn(returningValue)) {
+            StringBuilder logBuilder = new StringBuilder();
+            logMethod(joinPoint, logBuilder);
+            logReturn(returningValue, logBuilder, methodLogger);
+            logSuccessMessage(methodLogger, logBuilder);
+            createLog(joinPoint, logBuilder);
+        }
+    }
+
+    @AfterThrowing(value = "@annotation(methodLogger))", throwing = "exception")
+    public void methodExceptionLogger(JoinPoint joinPoint, MethodLogger methodLogger, Exception exception) {
+        StringBuilder logBuilder = new StringBuilder();
+        logMethod(joinPoint, logBuilder);
+        logErrorMessage(methodLogger, logBuilder,exception);
+        createLog(joinPoint, logBuilder);
+    }
+
+    private boolean hasReturn(Object returningValue) {
+        return returningValue != null;
+    }
+
+
+
+    private void logReturn(Object returningValue, StringBuilder logBuilder, MethodLogger methodLogger) {
+            logBuilder.append("r=");
+            logBuilder.append(returningValue);
+            logBuilder.append(" ");
+    }
+
+    private void createLog(JoinPoint joinPoint, StringBuilder logBuilder) {
+        Logger log = LoggerFactory.getLogger(joinPoint.getTarget().getClass());
+        log.info(logBuilder.toString());
+    }
+
+    private void logSuccessMessage(MethodLogger methodLogger, StringBuilder logBuilder) {
+        String msgOnSuccess = methodLogger.msgOnSuccess();
+        if (!msgOnSuccess.isEmpty()) {
+            logBuilder.append(msgOnSuccess);
+            logBuilder.append(" ");
+        }
+    }
+
+    private void logErrorMessage(MethodLogger methodLogger, StringBuilder logBuilder, Exception exception) {
+        String msgOnException = methodLogger.msgOnException();
+        if (!msgOnException.isEmpty()) {
+            logBuilder.append(msgOnException);
+            logBuilder.append(": \n");
+        }
+        logBuilder.append(exception.getMessage());
+        exception.printStackTrace();
+    }
+
+    private static void logArgs(JoinPoint joinPoint, StringBuilder logBuilder, MethodLogger methodLogger){
+        if (methodLogger.logAllArgs()) {
+            logAllArgs(joinPoint, logBuilder);
+        } else {
+            logOptionalArgs(joinPoint, logBuilder);
+        }
+    }
+
+    private static void logOptionalArgs(JoinPoint joinPoint, StringBuilder logBuilder) {
+        for (int i = 0; i < joinPoint.getArgs().length; i++) {
+            boolean hasArgumentLogger = hasArgumentLogger(joinPoint, i);
+            if (hasArgumentLogger) {
+                appendArgLog(joinPoint.getArgs()[i], logBuilder);
+            }
+        }
+    }
+
+    private static void appendArgLog(Object object, StringBuilder logBuilder) {
+        logBuilder.append("a=");
+        logBuilder.append(object);
+        logBuilder.append(" ");
+    }
+
+    private static boolean hasArgumentLogger(JoinPoint joinPoint, int i) {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Parameter[] parameters = signature.getMethod().getParameters();
+        return Stream.of(parameters[i].getAnnotations())
+                .anyMatch(annotation -> annotation.annotationType().isAssignableFrom(ArgumentLogger.class));
+    }
+
+    private static void logAllArgs(JoinPoint joinPoint, StringBuilder logBuilder) {
+        for (Object object : joinPoint.getArgs()) {
+            appendArgLog(object, logBuilder);
+        }
+    }
+
+    private static void logMethod(JoinPoint joinPoint, StringBuilder logBuilder) {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method method = signature.getMethod();
+        String methodName = method.getName();
+
+        logBuilder.append("m=");
+        logBuilder.append(methodName);
+        logBuilder.append(" ");
+    }
+}
